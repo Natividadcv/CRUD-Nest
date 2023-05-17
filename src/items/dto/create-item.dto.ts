fed63b853994c103b2672fbd3535b731ca5e1e11
Natividadcv <cv0195042019@unab.edu.sv>
2023-05-16 21:42:07 -0600

    Feat: Validacion de campos

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,14 @@
+import { IsNotEmpty,Length,IsNumber } from "class-validator";
+
 export class CreateItemDto {
+    @IsNotEmpty() // TODO: Esto es para que no se pueda enviar un campo vacio
+    @Length(3, 20) // TODO: Esto es para que el campo tenga un minimo de 3 caracteres y un maximo de 20
     name: string;
+
+    @IsNotEmpty()
+    @Length(3, 500)
     description: string;
+    
+    @IsNumber()
     userId: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // TODO https://docs.nestjs.com/openapi/introduction
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,10 +13,9 @@ async function bootstrap() {
   .addTag('items')
   .build();
 const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('documentacion', app, document);
+    SwaggerModule.setup('documentacion', app, document);
+    app.useGlobalPipes(new ValidationPipe());
 
-
-
-  await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
