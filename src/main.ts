d353f8f9e14a942758d3c479fe737c3cc5cc9108
Natividadcv <cv0195042019@unab.edu.sv>
2023-05-16 19:06:59 -0600

    feat: segundo commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // TODO https://docs.nestjs.com/openapi/introduction
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Mi API documentacion')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('items')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('documentacion', app, document);
+
+
+
   await app.listen(3000);
 }
 bootstrap();
