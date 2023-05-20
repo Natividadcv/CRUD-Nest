fb8d1a97859ba76f6d3a9cacd662a8a0a4500238
Natividadcv <cv0195042019@unab.edu.sv>
2023-05-19 23:48:38 -0600

    feat: Busquesda de paises

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { ItemsModule } from './items/items.module';
 import { PrismaService } from './items/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { CountryModule } from './country/country.module';
 
 @Module({
   imports: [
     ItemsModule,
     AuthModule,
-    ProductsModule
+    ProductsModule,
+    CountryModule
 ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,18 @@ async function bootstrap() {
   .setDescription('The cats API description')
   .setVersion('1.0')
   .addTag('items')
+  .addTag('country')
   .build();
+
+//   app.useGlobalPipes(
+//     new ValidationPipe({
+//         whitelist: true, // TODO: Esto es para que solo se acepten los campos que estan en el DTO
+//         forbidNonWhitelisted: true, // TODO: Esto es para que si se envia un campo que no esta en el DTO, se rechace la peticion
+//         transform: true, // TODO: Esto es para que los parametros que vienen en la peticion se transformen al tipo de dato que se espera en el DTO
+//     })
+//   )
+
+
 const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('documentacion', app, document);
     app.useGlobalPipes(new ValidationPipe());
@@ -20,3 +31,5 @@ const document = SwaggerModule.createDocument(app, config);
     await app.listen(3000);
 }
 bootstrap();
+
+// Los parametros que viejan en Res que viajan en http viene en string, por lo que hay que parsearlos a number
